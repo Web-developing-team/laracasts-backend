d607287374bad5f8abbfadb13be80b0a2f8f0423
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-08-21 14:13:43 +0430

    create admin routes middleware

---
+++ routes/admin.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\Admin\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function() {
+
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::middleware('auth:sanctum')->group(function() {
+
+        Route::get('check', [AuthController::class, 'check']);
+
+        Route::post('logout', [AuthController::class, 'logout']);
+
+    });
+
+
+});
