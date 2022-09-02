2e48a45c51bd5a10cbc419c30b99f0a18840b4b9
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-09-02 19:38:03 +0430

    complete login request validation

--- app/Http/Requests/Admin/LoginRequest.php
+++ app/Http/Requests/Admin/LoginRequest.php
@@ -24,7 +24,8 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'username' => 'required|min:6|max:255',
+            'password' => 'required|min:6|max:255',
         ];
     }
 }
